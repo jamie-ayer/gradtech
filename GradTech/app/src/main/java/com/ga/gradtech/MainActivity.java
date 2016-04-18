af281cb111ca0428166f75ed545b6bc0420350e5
Jamie Ayer <jameayer@gmail.com>
2016-04-18 10:08:08 -0700

    Added Libraries

--- GradTech/app/src/main/java/com/ga/gradtech/MainActivity.java
+++ GradTech/app/src/main/java/com/ga/gradtech/MainActivity.java
@@ -2,12 +2,22 @@ package com.ga.gradtech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.rv)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
     }
 }
