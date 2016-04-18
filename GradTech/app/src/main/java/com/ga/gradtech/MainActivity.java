79b106be65f8d96384f95f146dc463ea4e9ee5fc
Jacob Milling <JacobDexter-Milling@Jacobs-MacBook-Air.local>
2016-04-18 11:05:04 -0700

    Created twitter api keys

--- GradTech/app/src/main/java/com/ga/gradtech/MainActivity.java
+++ GradTech/app/src/main/java/com/ga/gradtech/MainActivity.java
@@ -6,8 +6,16 @@ import android.support.v7.widget.RecyclerView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = ApiKeys.twitter_key;
+    private static final String TWITTER_SECRET = ApiKeys.twitter_secret;
+
 
     @Bind(R.id.rv)
     RecyclerView recyclerView;
@@ -15,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
