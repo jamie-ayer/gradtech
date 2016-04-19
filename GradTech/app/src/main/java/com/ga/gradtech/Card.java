fad40a4b1dc821bdd915f8084213e30eaa9e30fd
Jamie Ayer <jameayer@gmail.com>
2016-04-19 16:16:25 -0700

    RecyclerView takes card objects instead of viewholders

--- GradTech/app/src/main/java/com/ga/gradtech/Card.java
+++ GradTech/app/src/main/java/com/ga/gradtech/Card.java
@@ -9,17 +9,17 @@ import android.widget.TextView;
  * Created by JamieAyer on 4/18/16.
  */
 
-public class Card extends RecyclerView.ViewHolder {
+public class Card {
 
-    private TextView mCompanyName;
-    private TextView mLocation;
+    private String mCompanyName;
+    private String mLocation;
     private ImageView photoId;
 
-    public TextView getmCompanyName() {
+    public String getmCompanyName() {
         return mCompanyName;
     }
 
-    public TextView getmLocation() {
+    public String getmLocation() {
         return mLocation;
     }
 
@@ -27,11 +27,11 @@ public class Card extends RecyclerView.ViewHolder {
         return photoId;
     }
 
-    public void setmCompanyName(TextView mCompanyName) {
+    public void setmCompanyName(String mCompanyName) {
         this.mCompanyName = mCompanyName;
     }
 
-    public void setmLocation(TextView mLocation) {
+    public void setmLocation(String mLocation) {
         this.mLocation = mLocation;
     }
 
@@ -39,10 +39,10 @@ public class Card extends RecyclerView.ViewHolder {
         this.photoId = photoId;
     }
 
-    public Card(View view) {
-        super(view);
-        mCompanyName = (TextView)view.findViewById(R.id.company_name);
-        mLocation = (TextView)view.findViewById(R.id.company_location);
-        photoId = (ImageView)view.findViewById(R.id.company_photo);
-    }
+//    public Card(View view) {
+//        super(view);
+//        mCompanyName = (TextView)view.findViewById(R.id.company_name);
+//        mLocation = (TextView)view.findViewById(R.id.company_location);
+//        photoId = (ImageView)view.findViewById(R.id.company_photo);
+//    }
 }
--- GradTech/app/src/main/java/com/ga/gradtech/Card2.java
+++ GradTech/app/src/main/java/com/ga/gradtech/Card2.java
@@ -9,17 +9,17 @@ import android.widget.TextView;
  * Created by JamieAyer on 4/18/16.
  */
 
-public class Card2 extends RecyclerView.ViewHolder {
+public class Card2 {
 
-    private TextView mCompanyName;
-    private TextView mLocation;
+    private String mCompanyName;
+    private String mLocation;
     private ImageView photoId;
 
-    public TextView getmCompanyName() {
+    public String getmCompanyName() {
         return mCompanyName;
     }
 
-    public TextView getmLocation() {
+    public String getmLocation() {
         return mLocation;
     }
 
@@ -27,22 +27,15 @@ public class Card2 extends RecyclerView.ViewHolder {
         return photoId;
     }
 
-    public void setmCompanyName(TextView mCompanyName) {
+    public void setmCompanyName(String mCompanyName) {
         this.mCompanyName = mCompanyName;
     }
 
-    public void setmLocation(TextView mLocation) {
+    public void setmLocation(String mLocation) {
         this.mLocation = mLocation;
     }
 
     public void setPhotoId(ImageView photoId) {
         this.photoId = photoId;
-    }
-
-    public Card2(View view) {
-        super(view);
-        mCompanyName = (TextView)view.findViewById(R.id.company_name2);
-        mLocation = (TextView)view.findViewById(R.id.company_location2);
-        photoId = (ImageView)view.findViewById(R.id.company_photo2);
     }
 }
