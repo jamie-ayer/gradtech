2e65b2b68efd2b038d1b237b1bcf33de1903ceb5
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-04-20 12:38:55 -0700

    Started MeetupLoginFragment

--- GradTech/app/src/main/java/com/ga/gradtech/Cards/Meetup/MeetupLoginFragment.java
+++ GradTech/app/src/main/java/com/ga/gradtech/Cards/Meetup/MeetupLoginFragment.java
@@ -1,9 +1,26 @@
 package com.ga.gradtech.Cards.Meetup;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebViewFragment;
 
 /**
  * Created by leisforkokomo on 4/20/16.
  */
 public class MeetupLoginFragment extends WebViewFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    
+
 }
